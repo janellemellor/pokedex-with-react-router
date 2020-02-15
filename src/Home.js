fc4185b5b9a37f8883bc81ca9b224fbb5ca78668
janelle mellor <janellemellor@gmail.com>
2020-02-15 15:28:08 -0800

    add pseudo-code to Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react'
 import request from 'superagent';
 import PokeItem from './PokeItem.js';
+//will need to inport searchPoke
 
 export default class Home extends Component {
+    //set state for poke and search query
+
+    //set up async function for componentdid Mount
+        //if search params match a name/id in the pokedex api
+        //get data from the api
+        //update state with the data results
+
+    //create an async function to get the data from the url (including search params)
+    //update state again with the poke data results
+
+    //create an event handler to set state based on the value(text) from the search bar
+
+
     render() {
         return (
             <div>
