5458399481b9a5be0bc9f920708688710b6843e6
janelle mellor <janellemellor@gmail.com>
2020-02-15 16:21:01 -0800

    add some functionality to PokeList.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import PokeList from './PokeList.js/index.js';
+import PokeList from './PokeList.js';
 
 export default class App extends Component {
   render() {
     return (
-      <div></div>
+      <div>
+        <PokeList />
+      </div>
     )};
 }
 
--- src/PokeList.js
+++ src/PokeList.js
@@ -1,35 +1,57 @@
 import React, { Component } from 'react'
 import request from 'superagent';
 import PokeItem from './PokeItem.js';
+import SearchPoke from './SearchPoke.js';
 //will need to inport searchPoke
 
 export default class PokeList extends Component {
-    //set state for poke and search query
+     //set state for poke and search query. ('pokemon' is key for name in API object)
+    state = {
+        pokemon: [],
+        searchQuery: this.props.match.params.pokemon,
+    }
+   
 
-    //set up async function for componentdid Mount
-        //if search params match a name/id in the pokedex api
-        //get data from the api
-        //update state with the data results
+    //set up async function for componentDidMount
+    async componentDidMount() {
+        if(this.props.match.params.pokemon) {
+            const pokeData = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex?pokemon=${this.props.match.params.pokemon}`)
 
-    //create an async function to get the data from the url (including search params)
-    //update state again with the poke data results
+            this.setState({ pokemon: pokeData.response.results })
+        }
+    }
 
-    //create an event handler to set state based on the value(text) from the search bar
+    handleSearch = async (e) => {
+        e.preventDefault();
+
+        const pokeData = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex?pokemon=${this.state.searchQuery}`)
+
+        this.setState({ pokemon: pokeData.response.results })
+
+        //history.push is a react router method
+        this.props.history.push(this.state.searchQuery)
+        }
 
 
     render() {
         return (
             <div>
                 <header>Pokedex
-                    {/*add in search component here  */}
+                    <SearchPoke
+                        searchQuery={this.state.searchQuery} 
+                        onChange={e => this.setState({ searchQuery: e.target.value })}
+                        handleSearch={this.handleSearch}
+                    />
                 </header>
 
                 <main>
                     <ul>
-                        {/* .map list of pokemon here. and add links to detail page here? and insert pokeItem here */
-                        <PokeItem poke={poke} />
+                         {this.state.pokemon.map(poke => 
+                         //add router link here?
+                         <PokeItem poke={poke} />
+                         )}
                         
-                        }
+                         
                             
                     </ul>
                 </main>
