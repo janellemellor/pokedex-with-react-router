46a464f2f1e5ad93010168e749aee7414ce707d3
janelle mellor <janellemellor@gmail.com>
2020-02-16 08:38:27 -0800

    add pseudo-code to detail page

--- src/Detail.js
+++ src/Detail.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react'
 import PokeItem from './PokeItem.js';
+import { getPoke } from './pokedexApi.js';
 
 export default class Detail extends Component {
+    //set state for poke
+
+    //add componentDidMount with async/await get pokemon data from api
+
+    //update state with poke data
+
     render() {
+        //set up state for poke below
+
         return (
             <div>
                 <PokeItem poke={poke}/>
